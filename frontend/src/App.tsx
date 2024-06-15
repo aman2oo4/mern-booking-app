e4b3b0aabe13586179cddb2bcfc960d5619c0641
aman <code.amanjain@gmail.com>
2024-06-15 17:27:48 +0530

    added search feature and e2e test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from './pages/AddHotel';
 import { useAppContext } from "./contexts/AppContext"
 import MyHotels from './pages/MyHotels';
 import EditHotel from './pages/EditHotel';
+import Search from './pages/Search';
 
 
 
@@ -25,8 +26,9 @@ function App() {
             <p>Home Page</p>
           </Layout>}
         />
+
         <Route path='/search' element={<Layout>
-          <p>Search Page</p>
+          <Search/>
         </Layout>} />
 
         <Route path='/register' element={
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { AppContextProvider } from './contexts/AppContext.tsx'
+import { SearchContextProvider } from './contexts/SearchContext.tsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 0,
+      refetchOnWindowFocus: false,
     },
   },
 });
@@ -17,7 +19,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
-        <App />
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
       </AppContextProvider>
     </QueryClientProvider>
   </React.StrictMode>,
